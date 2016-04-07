8186a4b46b46cad92730be89bb2b71322a86ca1e
arzon94 <andyp3rkins@gmail.com>
2016-04-07 15:31:41 -0600

    Added files via upload

--- mailbox.js
+++ mailbox.js
@@ -1,1 +1,10 @@
 // js
+$(document).ready(function(){
+    $("a").mouseenter(function(){
+        $(".header").fadeTo("slow", 0.6);
+        ("a").animate({fontSize: '3em'});
+    });
+    $("a").mouseleave(function(){
+        $(".header").fadeTo("slow", 1);
+    });
+});
